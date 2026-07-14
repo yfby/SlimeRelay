c2a2143d5133a81b21186d9944483acdfe8fc842
yfby <harveykyleybanez@gmail.com>
2026-07-15 01:38:19 +0800

    chore: base android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.github.yfby.slimerelay"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.github.yfby.slimerelay"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
